72c2746ab3d584821784c4e2bf8f34048e06d8a5
FSC711300 <3082920551@qq.com>
2024-08-10 22:13:34 +0800

    modified:   index.html 	modified:   scripts/calculate_video_size.js

--- scripts/calculate_video_size.js
+++ scripts/calculate_video_size.js
@@ -1,2 +1,4 @@
-var vid = document.getElementById("welcome-video");
-vid.style.height = parseInt(window.getComputedStyle(vid).width) / 16 * 9 + "px";
+setInterval(function () {
+    var vid = document.getElementById("welcome-video");
+    vid.style.height = parseInt(window.getComputedStyle(vid).width) / 16 * 9 + "px";
+},1000)
